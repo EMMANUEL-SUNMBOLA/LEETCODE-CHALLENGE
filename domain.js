fb2bc2e3ece3e26d78872ef07d5efa6a3cbbb3d2
ADE DAYO <adedayoemmanuel729@gmail.com>
2023-11-16 11:27:21 +0100

    domain.js

---
+++ domain.js
@@ -0,0 +1,18 @@
+// function to extract the domain name of a provided link
+
+function domainName(url){
+    let newArr = url.split("/");
+    for(let i = 0; i < newArr.length; i++){
+        if(newArr[i].includes(".com")){
+            if((newArr[i].includes("www."))){
+              let newerArr = newArr[i].split(".");
+              return newerArr[1];
+            }else{
+                let newerArr = newArr[i].split(".");
+                return newerArr[0];
+            }
+        }
+    }
+}
+
+console.log(domainName("http://www.zombie-bites.com"));
